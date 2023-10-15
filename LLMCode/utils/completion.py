b143f39c0b8b651f87ad8cb879665181b9b7931e
atos <fjganan14@gmail.com>
2023-10-15 14:41:47 +0200

    Added functionality in run_with_timeout to habdle others exceptions raised by the target function

--- LLMCode/utils/completion.py
+++ LLMCode/utils/completion.py
@@ -14,10 +14,17 @@ def run_with_timeout(target_function, args=None, timeout=30):
     try:
         result_value = result.get(timeout=timeout)
         return result_value
-    except multiprocessing.TimeoutError:
-        LOGGER.info(
-            f"{ANSI_CODE['yellow']}\r⚠ The completion could not be done. {target_function} response lasted more than {timeout} seconds, which is the limit."
-        )
+    except (
+        Exception
+    ) as e:  # multiprocessing.TimeoutError or openai.error.ServiceUnavailableError:
+        if isinstance(e, multiprocessing.TimeoutError):
+            LOGGER.info(
+                f"{ANSI_CODE['yellow']}\r⚠ The completion could not be done. {target_function} response lasted more than {timeout} seconds, which is the limit."
+            )
+        else:
+            LOGGER.info(
+                f"{ANSI_CODE['yellow']}\r⚠ The completion could not be done. {target_function} raised the exception {e}"
+            )
         pool.terminate()
         return None
     finally:
@@ -32,10 +39,13 @@ def get_completion_openai(prompt):
         for key, value in vars(completion_params).items()
         if not key.startswith("__")
     }
-    chat_completion = openai.ChatCompletion.create(
-        messages=[{"role": "user", "content": prompt}], **params
+    return (
+        openai.ChatCompletion.create(
+            messages=[{"role": "user", "content": prompt}], **params
+        )
+        .choices[0]
+        .message.content
     )
-    return chat_completion.choices[0].message.content
 
 
 # The completion function to use
