63f021dcc1e4d4d7eab84afc07b96a1d4d800410
fjganan14 <fjganan@movumtech.com>
2024-07-22 09:44:27 +0200

    Modified `gpt-3.5-turbo` by `gpt-4o-mini` model

--- LLMCode/cfg/completion_params.py
+++ LLMCode/cfg/completion_params.py
@@ -1,5 +1,5 @@
 completion_function = "get_completion_openai"
 # See https://platform.openai.com/docs/api-reference/chat/create
 #         for a list of valid parameters.
-model = "gpt-3.5-turbo"
+model = "gpt-4o-mini"
 max_tokens = 600
